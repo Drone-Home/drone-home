1fd09a9a1600b3b00d370b66f41e37844470177e
Dominik <dominik.kapuscinski3@gmail.com>
2024-10-25 11:13:30 -0400

    Added script to source, build, and run a node

--- ros2_ws/build/drone_home/prefix_override/sitecustomize.py
+++ ros2_ws/build/drone_home/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/pi/ros2_ws/install/drone_home'
+    sys.prefix = sys.exec_prefix = '/home/pi/git/drone-home/ros2_ws/install/drone_home'
---
+++ ros2_ws/source_build_run.sh
@@ -0,0 +1,2 @@
+source install/local_setup.bash
+colcon build && ros2 run drone_home imu_publisher 
