221db5c6a6d70c1ec55017be48042ce730904825
SonnyEclipsed <119905600+SonnyEclipsed@users.noreply.github.com>
2025-01-29 15:12:12 -0500

    Update predict.py

--- YOLOv11_custom/predict.py
+++ YOLOv11_custom/predict.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model0 = YOLO(r"C:\Users\conno\OneDrive\Documents\ASPRING25\AICAMERA\OpenCV\YOLOv11_custom\yolov11_custom.pt")
+model0 = YOLO("yolov11_custom.pt")
 model0.predict(source = "0", show=True, save=True, conf=0.7, line_width=2,
                save_crop = False, save_txt = False, show_labels = True, show_conf = True, classes=[0])
