9480a0c4630a0a79cdb8be8554297aaefa69aeff
Dominik Kapuscinski <dominik.kapuscinski3@gmail.com>
2025-02-01 00:18:04 -0500

    gitignore repo

--- drone-home/ros2_ws/build/drone_home/prefix_override/sitecustomize.py
+++ drone-home/ros2_ws/build/drone_home/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/workspaces/isaac_ros-dev/drone-home/ros2_ws/install/drone_home'
+    sys.prefix = sys.exec_prefix = '/workspaces/drone_home-dev/drone-home/ros2_ws/install/drone_home'
